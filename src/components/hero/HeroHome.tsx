8f70aea3ed134b9aa957a54920a9c3fafac459bc
manupor <135657852+manupor@users.noreply.github.com>
2025-06-07 23:50:33 -0600

    Update HeroHome.tsx

--- src/components/hero/HeroHome.tsx
+++ src/components/hero/HeroHome.tsx
@@ -15,7 +15,7 @@ const HeroHome = () => {
       text2: '200% SIGN UP BONUS!',
       text3: 'SAME DAY PAYOUTS, TRUE VIP EXPERIENCE',
       img_src: '/hero/01.png',
-      mobile_img_src: '/hero/mobile/MLB-RUBY.jpg',
+      mobile_img_src: '/hero/mobile/MLB_RUBY.jpg',
     },
     {
       text1: 'Win More, Get Paid Faster',
